d5a3c4dbccde5425186a04f3b8e437485e9c7457
Michael Ostermeier <mosterme@users.noreply.github.com>
2025-08-24 09:58:39 +0200

    Removed intermediate step.

--- sortme.sh
+++ sortme.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-# SAX=/usr/share/java/Saxon-HE.jar
-SRC=~/.var/app/de.mediathekview.MediathekView/config/mediathek3/mediathek.xml
-XML=mediathek-origin.xml
+XML=~/.var/app/de.mediathekview.MediathekView/config/mediathek3/mediathek.xml
 XSL=mediathek-sorter.xsl
 OUT=mediathek-sorted.xml
 
-echo -n copy\ && cp -v ${SRC} ${XML} &&
+# SAX=/usr/share/java/Saxon-HE.jar
 # echo exec \'java net.sf.saxon.Transform -xsl:${XSL} -s:${XML} -o:${OUT}\' &&
 # java -cp ${SAX}  net.sf.saxon.Transform -xsl:${XSL} -s:${XML} -o:${OUT} &&
 echo exec \'xsltproc -o ${OUT} ${XSL} ${XML}\' &&
 xsltproc -o ${OUT} ${XSL} ${XML} &&
-echo -n copy\ && cp -v ${OUT} ${SRC}
+echo -n copy\ && cp -v ${OUT} ${XML}
