4ceb72aa3a2a28b6c510e690c7df79f481ea848d
Michael Ostermeier <mosterme@users.noreply.github.com>
2025-08-24 20:44:19 +0200

    Removed saxon completely.

--- sortme.sh
+++ sortme.sh
@@ -4,9 +4,6 @@ XML=~/.var/app/de.mediathekview.MediathekView/config/mediathek3/mediathek.xml
 XSL=mediathek-sorter.xsl
 OUT=mediathek-sorted.xml
 
-# SAX=/usr/share/java/Saxon-HE.jar
-# echo exec \'java net.sf.saxon.Transform -xsl:${XSL} -s:${XML} -o:${OUT}\' &&
-# java -cp ${SAX}  net.sf.saxon.Transform -xsl:${XSL} -s:${XML} -o:${OUT} &&
 echo exec \'xsltproc -o ${OUT} ${XSL} ${XML}\' &&
 xsltproc -o ${OUT} ${XSL} ${XML} &&
 echo -n copy\ && cp -v ${OUT} ${XML}
